de5489e3a14538f9386e78d02d694dd7e2ff7d8b
Spidey7-AI <dhruvnautiyal24610@gmail.com>
2024-08-24 19:40:47 +0530

    added

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import time
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
+
+def response_generator(chain, prompt):
+    for word in chain.stream({"question": prompt}):
+        yield word + ""
+        time.sleep(0.05)
+
+def chat_strat(chain):
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    # Accept user input
+    if prompt := st.chat_input("What is up?"):
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # Add user message to chat history
+        # output = chain.invoke({"question": prompt})
+        with st.chat_message("assistant"):
+            response = st.write_stream(response_generator(chain, prompt))
+          
+            
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+
+model = OllamaLLM(model="mistral:7b")
+
+
+selected_type = add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Personal Trainer", "Pyschologist")
+)
+
+if selected_type == "Personal Trainer":
+    st.title("Welcome To Personal Trainer!")
+    template = """
+        your a personal trainer with extensive experience in helping clients achieve their fitness goals, how would you approach {question}?
+        Please provide detailed insights, including any recommended exercises, nutrition tips, and motivational strategies that could be useful
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+    chain_person_trainer = prompt | model
+    chat_strat(chain_person_trainer)
+elif  selected_type=="Pyschologist":
+    st.title("Welcome To Physcologist!")
+    template ="""
+        "Your a psychologist, please provide insights on the following question: {question}. 
+        Include relevant psychological concepts, coping strategies, and any advice that may help individuals dealing with this issue
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+    chain_physco = prompt | model
+    chat_strat(chain_physco)
+
+
+# with st.chat_message("user"):
+#     st.write("Hello 👋")
+
+
