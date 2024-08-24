62ead11f7d7ca19f8406eea45d675dedd64e8f38
Anurich <nautiyalanupam98@gmail.com>
2024-08-24 23:24:16 +0530

    added

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ from langchain_huggingface.llms import HuggingFacePipeline
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers import pipeline
 
-model = AutoModelForCausalLM.from_pretrained("gpt2")
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
+model = AutoModelForCausalLM.from_pretrained("openai-community/gpt2")
+tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2")
 text_generation_pipeline = pipeline(
     model=model,
     tokenizer=tokenizer,
